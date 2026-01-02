6863faf05c86ca6656794d8c16602be9be05162a
Aditya-Gupta23 <9555.adityagupta@gmail.com>
2026-01-02 15:59:24 +0530

    Added EmailOTP

--- backend/ocr_backend/verify_user/models.py
+++ backend/ocr_backend/verify_user/models.py
@@ -77,3 +77,14 @@ class PassportRecord(models.Model):
     verification_results = models.JSONField(blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+class EmailOTP(models.Model):
+    email = models.EmailField()
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_valid(self):
+        return timezone.now() <= self.created_at + timedelta(minutes=10)
+
+    def __str__(self):
+        return f"{self.email} - {self.otp}"
